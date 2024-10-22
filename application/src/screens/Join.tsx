7b370586066c742069050a0fa25a18b40681dbb8
김희은 <kimhe12121@gmail.com>
2024-10-22 14:23:34 +0900

    css

--- application/src/screens/Join.tsx
+++ application/src/screens/Join.tsx
@@ -6,6 +6,7 @@ import CustomButton from '../components/CustomButton';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
 import api from '../../axios'
+import Header from '../components/Header';
 
 
 type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
@@ -15,7 +16,7 @@ interface Props {
   csrfToken: string | null;
 }
 
-const Join : React.FC<Props> = ({csrfToken}) => {
+const Join: React.FC<Props> = ({ csrfToken }) => {
   
   const [id, setId] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -36,10 +37,10 @@ const Join : React.FC<Props> = ({csrfToken}) => {
     }
 
     const res = await api.post('/user/joindata', {
-      email : email,
-      password : password
+      email: email,
+      password: password
     },
-    {headers : {'X-CSRF-Token' : csrfToken}, withCredentials : true}
+      { headers: { 'X-CSRF-Token': csrfToken }, withCredentials: true }
   )
     navigation.navigate('Login');
   };
@@ -51,14 +52,14 @@ const Join : React.FC<Props> = ({csrfToken}) => {
     if (id.length > 4) { 
       const res = await api.post(
         '/user/idcheck',
-        {idck : id},
-        {headers : {'X-CSRF-Token': csrfToken}, withCredentials : true}
+        { idck: id },
+        { headers: { 'X-CSRF-Token': csrfToken }, withCredentials: true }
       )
       // 아이디 중복여부 체크
-      if(res.data.message === '중복') {
+      if (res.data.message === '중복') {
         setIdck(false)
         Alert.alert('중복', '이미 사용 중인 아이디입니다.')
-      } else if(res.data.message === '가능') {
+      } else if (res.data.message === '가능') {
         setIdck(true)
         Alert.alert('사용 가능한 아이디', '사용 가능한 아이디입니다.')
       }
@@ -68,6 +69,19 @@ const Join : React.FC<Props> = ({csrfToken}) => {
   }
 
   return (
+    <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={commonStyles.container}
+      resizeMode="cover"
+    >
+      <Header onBackPress={() => navigation.goBack()} />
+
+
+      <KeyboardAvoidingView
+        style={commonStyles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // iOS와 Android에 맞는 동작 설정
+      >
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
     <View style={commonStyles.container}>
       <View style={commonStyles.view1}>
         <TextInput
@@ -75,12 +89,14 @@ const Join : React.FC<Props> = ({csrfToken}) => {
           placeholder="ID를 입력해주세요."
           value={id}
           onChangeText={setId}
+                autoCapitalize="none"
         />
-        <TouchableOpacity
-          style={commonStyles.input2}
-          onPress={() => id_redundancy_check()}>
-          <Text>중복 확인</Text>
-        </TouchableOpacity>
+              <CustomButton
+                title="중복확인"
+                onPress={() => id_redundancy_check()}
+                style={commonStyles.smallButton} // 스타일 적용
+              />
+
       </View>
       <TextInput
         style={commonStyles.input}
@@ -103,45 +119,50 @@ const Join : React.FC<Props> = ({csrfToken}) => {
         value={name}
         onChangeText={setName}
       />
-      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginBottom: 20 }}>
         <TouchableOpacity
-          onPress={() => setGender('남성')}
-          style={[commonStyles.radioButton, gender === '남성' && commonStyles.activeButton]}>
-          <Text>남성</Text>
+                onPress={() => setGender('남')}
+                style={[commonStyles.radioButton, gender === '남' && commonStyles.activeButton]}>
+                <Text>남</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => setGender('여성')}
-          style={[commonStyles.radioButton, gender === '여성' && commonStyles.activeButton]}>
-          <Text>여성</Text>
+                onPress={() => setGender('여')}
+                style={[commonStyles.radioButton, gender === '여' && commonStyles.activeButton]}>
+                <Text>여</Text>
         </TouchableOpacity>
-      </View>
     </View>
       <TextInput
         style={commonStyles.input}
         placeholder="Email을 입력해주세요."
         value={email}
         onChangeText={setEmail}
+              keyboardType="email-address"
+              autoCapitalize="none"
       />
       <TextInput
         style={commonStyles.input}
         placeholder="생년월일을 입력해주세요. (예: 1990-01-01)"
         value={birth}
         onChangeText={setBirth}
+              keyboardType="numeric"
+              autoCapitalize="none"
       />
       <TextInput
         style={commonStyles.input}
         placeholder="핸드폰 번호를 입력해주세요."
         value={phone}
         onChangeText={setPhone}
+              keyboardType="phone-pad"
+              autoCapitalize="none"
       />
 
-      <CustomButton title="회원가입" onPress={handleJoin} />
-      <CustomButton title="로그인" onPress={() => navigation.navigate('Login')} />
+            <CustomButton style={commonStyles.fullWidthButton} title="회원가입" onPress={handleJoin} />
+            <CustomButton style={commonStyles.smallButton} title="로그인" onPress={() => navigation.navigate('Login')} />
     </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </ImageBackground>
   );
 };
 
 export default Join;
-
-
 
