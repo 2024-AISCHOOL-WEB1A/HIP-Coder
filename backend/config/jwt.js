1b519d46108d0ceb63b132a2a3aaf58ff6049a85
gjseh115 <gjseh115@gmail.com>
2024-11-04 16:14:48 +0900

    .

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -1,20 +1,19 @@
+const express = require("express")
+const bodyParser = require("body-parser")
 const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: '.env' });
 const path = require('path');
 
+const app = express();
+
 // 환경변수에서 키 로드
-const secretkey = process.env.JWT_KEY
+const SECRET_KEY = process.env.SECRET_KEY;
+const TOKEN_EXPIRY = process.env.TOKEN_EXPIRY
 
-// JWT 생성 (로그인 시)
-const token = jwt.sign({ userId: '123' }, secretKey, { expiresIn: '1h' });
-console.log(token);
+// 본문 데이터 요청 미들웨어
+app.use(bodyParser.json());
 
-// JWT 검증 (요청 시)
-try {
-  const decoded = jwt.verify(token, secretKey);
-  console.log(decoded); // 검증된 사용자 정보
-} catch (error) {
-  console.error('토큰 검증 실패:', error);
-}
 
-// 나중에 수정필요
+app.post("/Login", (req, res) => {
+  const { id, password } = req.body;
+})
