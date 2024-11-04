62187998e8bb0320f0035eaa504a69374cc40719
gjseh115 <gjseh115@gmail.com>
2024-11-04 21:00:04 +0900

    로그인 jwt 생성, 저장 -헌도 1. jwt.js 로그인시 토큰 생성, 검증 함수 2. userRouter 로그인 성공시 jwt 토큰 생성 3. login, Home.tsx 로그인 토큰 발급, 로그아웃 토큰 삭제, async storage 토큰 저장, 토큰 삭제 console.log

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -1,19 +1,25 @@
 const express = require("express")
-const bodyParser = require("body-parser")
 const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: '.env' });
 const path = require('path');
-
-const app = express();
 
 // 환경변수에서 키 로드
 const SECRET_KEY = process.env.SECRET_KEY;
 const TOKEN_EXPIRY = process.env.TOKEN_EXPIRY
 
-// 본문 데이터 요청 미들웨어
-app.use(bodyParser.json());
+// JWT 토큰 생성
+function generateToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn : TOKEN_EXPIRY });
+}
 
+// JWT 토큰 검증 함수
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  }catch (error) {
+    return null; // 유요하지 않은 토큰일 경우 null 반환
+  }
+}
 
-app.post("/Login", (req, res) => {
-  const { id, password } = req.body;
-})
+// 외부에서 사용할 수 있도록 함수 exports
+module.exports = { generateToken, verifyToken }
